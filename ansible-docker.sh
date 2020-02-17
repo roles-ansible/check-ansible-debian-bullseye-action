4ec3a8b88dbfc873939317f2598895e1e4be3f03
L3D <l3d@c3woc.de>
2020-02-17 13:15:05 +0100

    test roles with ansible

--- ansible-docker.sh
+++ ansible-docker.sh
@@ -3,93 +3,18 @@
 set -Eeuo pipefail
 set -x
 
-# Filter out arguments that are not available to this action
-# args:
-#   $@: Arguments to be filtered
-parse_args() {
-  local opts=""
-  while (( "$#" )); do
-    case "$1" in
-      -q|--quiet)
-        opts="$opts -q"
-        shift
-        ;;
-      -c)
-        opts="$opts -c $2"
-        shift 2
-        ;;
-      -p)
-        opts="$opts -p"
-        shift
-        ;;
-      -r)
-        opts="$opts -r $2"
-        shift 2
-        ;;
-      -R)
-        opts="$opts -R"
-        shift
-        ;;
-      -t)
-        opts="$opts -t $2"
-        shift 2
-        ;;
-      -x)
-        opts="$opts -x $2"
-        shift 2
-        ;;
-      --exclude)
-        opts="$opts --exclude=$2"
-        shift 2
-        ;;
-      --no-color)
-        opts="$opts --no-color"
-        shift
-        ;;
-      --parseable-severity)
-        opts="$opts --parseable-severity"
-        shift
-        ;;
-      --) # end argument parsing
-        shift
-        break
-        ;;
-      -*|--*=) # unsupported flags
-        >&2 echo "ERROR: Unsupported flag: '$1'"
-        exit 1
-        ;;
-      *) # positional arguments
-        shift  # ignore
-        ;;
-    esac
-  done
-
-  # set remaining positional arguments (if any) in their proper place
-  eval set -- "$opts"
-
-  echo "${opts/ /}"
-  return 0
-}
-
 # Generates client.
-# args:
-#   $@: additional options
 # env:
-#   [required] TARGETS : Files or directories (i.e., playbooks, tasks, handlers etc..) to be linted
-ansible::lint() {
+#   [required] TARGETS : Files or directories (i.e., roles, playbooks, tasks, handlers etc..) to be tested
+ansible::test() {
   : "${TARGETS?No targets to check. Nothing to do.}"
   : "${GITHUB_WORKSPACE?GITHUB_WORKSPACE has to be set. Did you use the actions/checkout action?}"
   pushd ${GITHUB_WORKSPACE}
 
-  local opts=$(parse_args "$@" || exit 1)
-
-  ansible-lint -v --force-color $opts ${TARGETS}
+  ansible -v localhost -m include_role -a name=${TARGETS}
 }
 
-
-args=("$@")
-
 if [ "$0" = "$BASH_SOURCE" ] ; then
-  >&2 echo -E "\nRunning Ansible Lint...\n"
-  ansible::lint ${args[@]}
+  >&2 echo -E "\nRunning Ansible debian check...\n"
+  ansible::test
 fi
