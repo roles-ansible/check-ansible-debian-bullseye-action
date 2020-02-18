636b073a475e01c42d040d580488895d479c795d
L3D <l3d@c3woc.de>
2020-02-18 10:39:25 +0100

    Support playbook checks too

--- ansible-docker.sh
+++ ansible-docker.sh
@@ -5,8 +5,35 @@ set -x
 
 # Generates client.
 # env:
-#   [required] TARGETS : Path to your ansible role you want to be tested. (e.g, './' or 'roles/my_role/') to be tested
-ansible::test() {
+#   [required] TARGETS : Path to your ansible role or to a playbook .yml file you want to be tested.
+#                       (e.g, './' or 'roles/my_role/' for roles or 'site.yml' for playbooks)
+
+ansible::prepare() {
+  : "${TARGETS?No targets to check. Nothing to do.}"
+  : "${GITHUB_WORKSPACE?GITHUB_WORKSPACE has to be set. Did you use the actions/checkout action?}"
+  pushd ${GITHUB_WORKSPACE}
+
+  # generate ansible.cfg
+  if [ ! -f ansible.cfg ]; then
+    echo -e """
+[defaults]
+inventory = host.ini
+nocows = True
+host_key_checking = False
+forks = 20
+fact_caching = jsonfile
+fact_caching_connection = $HOME/facts
+fact_caching_timeout = 7200
+stdout_callback = yaml
+ansible_python_interpreter=/usr/bin/python3
+ansible_connection=local
+""" | tee -a ansible.cfg
+  fi
+
+  # create host list
+  echo -e "[local]\nlocalhost ansible_python_interpreter=/usr/bin/python3 ansible_connection=local" | tee -a host.ini
+}
+ansible::test::role() {
   : "${TARGETS?No targets to check. Nothing to do.}"
   : "${GITHUB_WORKSPACE?GITHUB_WORKSPACE has to be set. Did you use the actions/checkout action?}"
   pushd ${GITHUB_WORKSPACE}
@@ -20,29 +47,34 @@ ansible::test() {
       - \""${TARGETS}"\"
     """ | tee -a deploy.yml
 
-  # generate ansible.cfg
-  echo -e """
-[defaults]
-inventory = host.ini
-nocows = True
-host_key_checking = False
-forks = 20
-fact_caching = jsonfile
-fact_caching_connection = $HOME/facts
-fact_caching_timeout = 7200
-stdout_callback = yaml
-ansible_python_interpreter=/usr/bin/python3
-ansible_connection=local
-""" | tee -a ansible.cfg
-
-  # create host list
-  echo -e "[local]\nlocalhost" | tee -a host.ini
+  # execute playbook
+  ansible-playbook \
+  --connection=local \ 
+  --inventory localhost, \
+  --limit localhost deploy.yml
+}
+ansible::test::playbook() {
+  : "${TARGETS?No targets to check. Nothing to do.}"
+  : "${GITHUB_WORKSPACE?GITHUB_WORKSPACE has to be set. Did you use the actions/checkout action?}"
+  pushd ${GITHUB_WORKSPACE}
 
   # execute playbook
-  ansible-playbook -vvv -i localhost deploy.yml
+  ansible-playbook \
+  --connection=local \ 
+  --inventory localhost, \
+  --limit localhost ${TARGETS} 
 }
+
 
 if [ "$0" = "$BASH_SOURCE" ] ; then
   >&2 echo -E "\nRunning Ansible debian check...\n"
-  ansible::test
+  ansible::prepare
+  if [[ "${TARGETS}" == *.yml ]]
+  then
+      echo -E "\nansible playbook detected\ninitialize playbook testing...\n"
+      ansible::test::playbook
+  else
+      echo -E "\nno playbook detected\ninitialize role testing...\n"
+      ansible::test::role
+  fi
 fi
